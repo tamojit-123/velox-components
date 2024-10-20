9617faa68609d6a1396a44a12d5f33d6814d259e
tamo <tamo@example.com>
2024-10-20 10:17:52 -0700

    gh-pages

--- src/page/homePage/Page.jsx
+++ src/page/homePage/Page.jsx
@@ -36,7 +36,7 @@ export const Page = () => {
         <ul>
           <li>
             Use a higher-level connected component. Storybook helps you compose
-            such data from the "args" of child component stories
+            such data from the &#34;args&#34; of child component stories
           </li>
           <li>
             Assemble data in the page component from your services. You can mock
